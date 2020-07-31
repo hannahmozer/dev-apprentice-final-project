41be7e36a38c96904d54e1eee95d8db70e8ab9e9
Hannah Mozer <50218230+hannahmozer@users.noreply.github.com>
2020-07-31 20:58:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={"In the weeks leading up to the collection presentation, Loïc Prigent spent time with Virginie Viard, her Studio and the Haute Couture ateliers to bring you a behind-the-scenes look at the creation process at CHANEL."}
       />
 
       <Carousel 
